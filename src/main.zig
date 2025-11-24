d66acb4548fe0b2f9fadd492a90261fc74043683
MikeDevThePole <mikepolethedev@gmail.com>
2025-11-24 19:26:04 +0100

    update

--- src/main.zig
+++ src/main.zig
@@ -2,68 +2,159 @@ const std = @import("std");
 
 const allocator = std.heap.page_allocator;
 
+const Limb = u64;
+
 const BigInt = struct {
-    limbs: []u64,
+    limbs: []Limb,
     sign: bool, // Signifies an infinite amount of ones after the base value
 
     pub const zero = BigInt{ .limbs = &.{}, .sign = false };
     pub const minus_one = BigInt{ .limbs = &.{}, .sign = true };
 
-    inline fn fillLimb(self: *const BigInt) u64 {
-        return @as(u64, @intFromBool(!self.sign)) -% 1;
-    }
-
-    pub fn grow(self: *BigInt) !void {
-        const new_limbs = try allocator.alloc(u64, self.limbs.len + 1);
-        @memcpy(new_limbs[0..self.limbs.len], self.limbs);
-        @memset(new_limbs[self.limbs.len..], self.fillLimb());
-        allocator.free(self.limbs);
-        self.limbs = new_limbs;
-    }
-
     pub fn fromInteger(value: i65) !BigInt {
-        const limbs = try allocator.alloc(u64, 1);
+        const limbs = try allocator.alloc(Limb, 1);
         limbs[0] = @truncate(@as(u65, @bitCast(value)));
         const sign = value < 0;
         return BigInt{ .limbs = limbs, .sign = sign };
     }
 
-    pub fn intoInteger(self: *const BigInt) i65 {
-        std.debug.assert(self.limbs.len <= 1);
-        const value = if (self.limbs.len > 0) self.limbs[0] else self.fillLimb();
-        const sign_bit = @as(u65, @intFromBool(self.sign)) << 64;
-        return @bitCast(sign_bit | value);
+    pub fn clone(self: *const BigInt) !BigInt {
+        const new_limbs = try allocator.dupe(Limb, self.limbs);
+        return BigInt{ .limbs = new_limbs, .sign = self.sign };
     }
 
-    pub fn shrink(self: *BigInt) !void {
+    pub fn deinit(self: *BigInt) void {
+        allocator.free(self.limbs);
+        self.* = undefined;
+    }
+
+    pub fn isZero(self: *const BigInt) bool {
+        return !self.sign and self.limbs.len == 0;
+    }
+
+    pub fn add(lhs: BigInt, rhs: BigInt) !BigInt {
+        const min_limbs = @min(lhs.limbs.len, rhs.limbs.len);
+        const max_limbs = @min(lhs.limbs.len, rhs.limbs.len);
+        const new_limbs = try allocator.alloc(Limb, @max(lhs.limbs.len, rhs.limbs.len));
+
+        var carry: u1 = 0;
+        for (0..min_limbs) |i| {
+            const sum1, const carry1 = @addWithOverflow(lhs.limbs[i], rhs.limbs[i]);
+            const sum2, const carry2 = @addWithOverflow(sum1, carry);
+            new_limbs[i] = sum2;
+            carry = carry1 | carry2;
+        }
+
+        if (lhs.limbs.len != rhs.limbs.len) {
+            const fill_value = if (lhs.limbs.len <= rhs.limbs.len) lhs.fillLimb() else rhs.fillLimb();
+            const longer_arr = if (lhs.limbs.len <= rhs.limbs.len) lhs.limbs else rhs.limbs;
+            for (min_limbs..max_limbs) |i| {
+                const sum1, const carry1 = @addWithOverflow(fill_value, longer_arr[i]);
+                const sum2, const carry2 = @addWithOverflow(sum1, carry);
+                new_limbs[i] = sum2;
+                carry = carry1 | carry2;
+            }
+        }
+
+        var sum = BigInt{ .limbs = new_limbs, .sign = undefined };
+
+        if (lhs.sign == rhs.sign) {
+            sum.sign = lhs.sign;
+            if (@intFromBool(lhs.sign) != carry) {
+                try sum.grow();
+                sum.limbs[sum.limbs.len - 1] = lhs.fillLimb() ^ 1;
+            }
+        } else {
+            sum.sign = carry == 0;
+            try sum.shrink();
+        }
+
+        return sum;
+    }
+
+    pub fn sub(lhs: BigInt, rhs: BigInt) !BigInt {
+        const min_limbs = @min(lhs.limbs.len, rhs.limbs.len);
+        const max_limbs = @min(lhs.limbs.len, rhs.limbs.len);
+        const new_limbs = try allocator.alloc(Limb, max_limbs);
+
+        var borrow: u1 = 0;
+
+        for (0..min_limbs) |i| {
+            const d1, const b1 = @subWithOverflow(lhs.limbs[i], rhs.limbs[i]);
+            const d2, const b2 = @subWithOverflow(d1, borrow);
+            new_limbs[i] = d2;
+            borrow = b1 | b2;
+        }
+
+        if (lhs.limbs.len != rhs.limbs.len) {
+            if (lhs.limbs.len <= rhs.limbs.len) {
+                const lhs_fill = lhs.fillLimb();
+                for (min_limbs..max_limbs) |i| {
+                    const d1, const b1 = @subWithOverflow(lhs_fill, rhs.limbs[i]);
+                    const d2, const b2 = @subWithOverflow(d1, borrow);
+                    new_limbs[i] = d2;
+                    borrow = b1 | b2;
+                }
+            } else {
+                const rhs_fill = rhs.fillLimb();
+                for (min_limbs..max_limbs) |i| {
+                    const d1, const b1 = @subWithOverflow(lhs.limbs[i], rhs_fill);
+                    const d2, const b2 = @subWithOverflow(d1, borrow);
+                    new_limbs[i] = d2;
+                    borrow = b1 | b2;
+                }
+            }
+        }
+
+        var result = BigInt{ .limbs = new_limbs, .sign = undefined };
+
+        if (lhs.sign != rhs.sign) {
+            result.sign = lhs.sign;
+            if (@intFromBool(lhs.sign) == borrow) {
+                try result.grow();
+                result.limbs[result.limbs.len - 1] = lhs.fillLimb() ^ 1;
+            }
+        } else {
+            result.sign = borrow != 0;
+            try result.shrink();
+        }
+
+        return result;
+    }
+
+    inline fn fillLimb(self: *const BigInt) Limb {
+        return @as(Limb, @intFromBool(!self.sign)) -% 1;
+    }
+
+    fn grow(self: *BigInt) !void {
+        const new_limbs = try allocator.alloc(Limb, self.limbs.len + 1);
+        @memcpy(new_limbs[0..self.limbs.len], self.limbs);
+        allocator.free(self.limbs);
+        self.limbs = new_limbs;
+    }
+
+    fn shrink(self: *BigInt) !void {
         const removable = self.fillLimb();
         var i = self.limbs.len;
-        while (i > 0) {
-            i -= 1;
-            if (self.limbs[i] != removable) {
+        while (i > 0) : (i -= 1) {
+            if (self.limbs[i - 1] != removable) {
                 break;
             }
         }
         if (i != self.limbs.len) {
-            const new_limbs = try allocator.alloc(u64, i);
+            const new_limbs = try allocator.alloc(Limb, i);
             @memcpy(new_limbs, self.limbs[0..i]);
             allocator.free(self.limbs);
             self.limbs = new_limbs;
         }
     }
 
-    pub fn deinit(self: *BigInt) void {
-        allocator.free(self.limbs);
-    }
-
-    pub fn bitwiseNot(self: *BigInt) void {
+    fn negMut(self: *BigInt) !void {
         self.sign = !self.sign;
         for (self.limbs) |*limb| {
             limb.* = ~limb.*;
         }
-    }
 
-    pub fn inc(self: *BigInt) !void {
         for (self.limbs) |*limb| {
             const new_value, const overflow = @addWithOverflow(limb.*, 1);
             limb.* = new_value;
@@ -77,68 +168,116 @@ const BigInt = struct {
 
         if (self.sign) {
             self.sign = false;
-            return; // At this point the value should be zero
+            return; // At this point the value should already have zero limbs
         }
 
         try self.grow();
         self.limbs[self.limbs.len - 1] = 1;
     }
 
-    pub fn neg(self: *BigInt) !void {
-        self.bitwiseNot();
-        try self.inc();
+    fn divModMut(self: *BigInt, divisor: Limb) !Limb {
+        var remainder: Limb = 0;
+        var i: usize = self.limbs.len;
+        while (i > 0) {
+            i -= 1;
+            const value = (@as(u128, remainder) << 64) | self.limbs[i];
+            self.limbs[i] = @intCast(value / divisor);
+            remainder = @intCast(value % divisor);
+        }
+        try self.shrink();
+        return remainder;
     }
 
-    pub fn add(self: *const BigInt, other: *const BigInt) !BigInt {
-        var a = self;
-        var b = other;
-        if (a.limbs.len > b.limbs.len) {
-            std.mem.swap(*const BigInt, &a, &b);
+    pub fn hex(self: BigInt) HexFormatter {
+        return HexFormatter{ .value = self };
+    }
+
+    pub fn decimal(self: BigInt) DecimalFormatter {
+        return DecimalFormatter{ .value = self };
+    }
+};
+
+const HexFormatter = struct {
+    value: BigInt,
+
+    pub fn tryWrite(self: HexFormatter, writer: *std.io.Writer) !void {
+        if (self.value.isZero()) {
+            try writer.writeByte('0');
+            return;
         }
 
-        const new_limbs = try allocator.alloc(u64, b.limbs.len);
+        var copy = try self.value.clone();
+        defer copy.deinit();
 
-        var carry: u1 = 0;
-        for (0..a.limbs.len) |i| {
-            const sum1, const carry1 = @addWithOverflow(a.limbs[i], b.limbs[i]);
-            const sum2, const carry2 = @addWithOverflow(sum1, carry);
-            new_limbs[i] = sum2;
-            carry = carry1 | carry2;
+        if (copy.sign) {
+            try copy.negMut();
+            try writer.writeByte('-');
         }
 
-        const a_fill = a.fillLimb();
-        for (a.limbs.len..b.limbs.len) |i| {
-            const sum1, const carry1 = @addWithOverflow(a_fill, b.limbs[i]);
-            const sum2, const carry2 = @addWithOverflow(sum1, carry);
-            new_limbs[i] = sum2;
-            carry = carry1 | carry2;
-        }
-
-        var sum = BigInt{ .limbs = new_limbs, .sign = undefined };
-
-        if (a.sign == b.sign) {
-            sum.sign = a.sign;
-            if (@intFromBool(a.sign) != carry) {
-                try sum.grow();
-                sum.limbs[sum.limbs.len - 1] = a_fill ^ 1;
+        var i: usize = copy.limbs.len;
+        var started = false;
+        while (i > 0) {
+            i -= 1;
+            const limb = copy.limbs[i];
+            var s: usize = 64;
+            while (s > 0) {
+                s -= 4;
+                const digit: u8 = @intCast((limb >> @intCast(s)) & 0xF);
+                if (!started) {
+                    if (digit == 0) continue; // skip leading zeros
+                    started = true;
+                }
+                try writer.writeByte("0123456789ABCDEF"[digit]);
             }
-        } else {
-            sum.sign = carry == 0;
-            try sum.shrink();
+        }
+    }
+
+    pub fn format(self: HexFormatter, writer: *std.io.Writer) std.io.Writer.Error!void {
+        return self.tryWrite(writer) catch error.WriteFailed;
+    }
+};
+
+const DecimalFormatter = struct {
+    value: BigInt,
+
+    pub fn tryWrite(self: DecimalFormatter, writer: *std.Io.Writer) !void {
+        if (self.value.isZero()) {
+            try writer.writeByte('0');
+            return;
         }
 
-        return sum;
+        var copy = try self.value.clone();
+        defer copy.deinit();
+
+        if (copy.sign) {
+            try copy.negMut();
+            try writer.writeByte('-');
+        }
+
+        var digits: std.ArrayList(u8) = .empty;
+        defer digits.deinit(allocator);
+
+        while (!copy.isZero()) {
+            const rem = try copy.divModMut(10);
+            try digits.append(allocator, @intCast(rem));
+        }
+
+        var i = digits.items.len;
+        while (i > 0) {
+            i -= 1;
+            try writer.writeByte('0' + digits.items[i]);
+        }
+    }
+
+    pub fn format(self: DecimalFormatter, writer: *std.Io.Writer) std.Io.Writer.Error!void {
+        return self.tryWrite(writer) catch error.WriteFailed;
     }
 };
 
 pub fn main() !void {
-    const a = try BigInt.fromInteger(std.math.minInt(i65));
-    const b = try BigInt.fromInteger(-1);
-    const sum = try BigInt.add(&a, &b);
-    std.debug.print("{X}\n", .{@as([]const u8, @ptrCast(sum.limbs))});
-    // var value = BigInt.zero;
-    // for (0..128) |_| {
-    //     try value.inc();
-    //     std.debug.print("{any}\n", .{value});
-    // }
+    const a = try BigInt.fromInteger(1);
+    const b = try BigInt.fromInteger(234);
+    const diff = try a.sub(b);
+    std.debug.print("{f}\n", .{diff.decimal()});
+    std.debug.print("Must be: {d}\n", .{1 - 234});
 }
